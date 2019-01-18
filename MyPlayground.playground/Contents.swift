b2cfefb22e3ddf4248062dca505f49f123579405
Menhaj <menhaj@Menhajs-MacBook-Pro.local>
2019-01-17 22:17:05 -0500

    commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit //import APIs
+
+var firstName = "Romeo";
+var lastName = "Lover";
+var welcome: String = firstName + " " + lastName + ", Welcome to Romeo and Juliet";
+print(welcome);
+
+
+//Types
+//Int, Double, Float, Bool,
+
+var name: String = "Dojo";
+var id: Int = 001;
+var salary: Double = 20304.32;
+var workHours: Float = 40.5;
+var ableToWork: Bool = true;
+
+
