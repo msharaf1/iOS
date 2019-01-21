553d9c6cfb8aad58789350190c74733b0a8152c9
Menhaj <menhaj@Menhajs-MacBook-Pro.local>
2019-01-21 11:00:43 -0500

    commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,64 +1,107 @@
-import UIKit //import APIs
+//import UIKit //import APIs
+//
+//var firstName = "Romeo";
+//var lastName = "Lover";
+//var welcome: String = firstName + " " + lastName + ", Welcome to Romeo and Juliet";
+//print(welcome);
+//
+//
+////Types
+////Int, Double, Float, Bool,
+//
+//var name: String = "Dojo";
+//var id: Int = 001;
+//var salary: Double = 20304.32;
+//var workHours: Float = 40.5;
+//var ableToWork: Bool = true;
+//
+//
+//let sID: Int = 1001;
+//var badgeId: Int = 2991;
+//print(sID, badgeId);
+//
+//badgeId = 3000;
+//print(badgeId);
+////sID = 3994; not changeable.
+//
+//var mArray = ["One"];
+//mArray.append("Hello");
+//
+//print(mArray);
+//
+//var myDictionary = ["one": 1];
+//myDictionary=["two": 2];
+//myDictionary=["two":0]
+//
+//print(myDictionary);
+//
+//var poem: String = "";
+//
+//poem = """
+//    A green land with beautifull water-falls.
+//
+//""";
+//
+//print(poem)
+//
+//
+////Conditionals
+//
+//var loteryNumber: Int = 5;
+//if loteryNumber == 5{
+//    print("you are the winner")
+//}else{
+//    print("unfortunately, you are not the winner!")
+//}
+//
+//var classNumber: Int = 10;
+//if classNumber >= 5{
+//    print("You'r classes start on JAN 10th")
+//}else if classNumber < 5{
+//    print("classes start on Feb 5")
+//}else{
+//    print("unknown class number")
+//}
+//
+//var jName: String = "James";
+//let jState: String = "VA";
+//var jId: Int = 101;
+//
+//if jId >= 100 && jName == "James" {
+//    print("\(jName) is a contractor.")
+//}
+//
+//for i in 1...5{
+//    print(i);
+//}
+//
+//var i = 1;
+//while i < 6{
+//    print(i);
+//    i = i+1;
+//}
 
-var firstName = "Romeo";
-var lastName = "Lover";
-var welcome: String = firstName + " " + lastName + ", Welcome to Romeo and Juliet";
-print(welcome);
 
+var type: String = "Rectangle";
+var description: String = "A quadrilateral with four right angles";
+var width: Float = 5.0;
+var height: Float = 10.5;
+var area: Double = Double(width) * Double(height);
+height += 1;
+width += 1;
+//area = width * height
+// Note how you can "interpolate" variables into Strings using the following syntax
 
-//Types
-//Int, Double, Float, Bool,
+print("The shape is a \(type) or \(description) with an area of \(area)")
 
-var name: String = "Dojo";
-var id: Int = 001;
-var salary: Double = 20304.32;
-var workHours: Float = 40.5;
-var ableToWork: Bool = true;
+//var h: Float = 0.0;
+//var w: Float = 0.0;
+//
+//var Rectangle: Float = h * w;
+//
+//var type: String = "Rectangle";
+//var description: String = "A quadrilateral with four right angles"
+//
 
+    //a + b + c + d;
 
-let sID: Int = 1001;
-var badgeId: Int = 2991;
-print(sID, badgeId);
-
-badgeId = 3000;
-print(badgeId);
-//sID = 3994; not changeable.
-
-var mArray = ["One"];
-mArray.append("Hello");
-
-print(mArray);
-
-var myDictionary = ["one": 1];
-myDictionary=["two": 2];
-myDictionary=["two":0]
-
-print(myDictionary);
-
-var poem: String = "";
-
-poem = """
-    A green land with beautifull water-falls.
-
-""";
-
-print(poem)
-
-
-//Conditionals
-
-var loteryNumber: Int = 5;
-if loteryNumber == 5{
-    print("you are the winner")
-}else{
-    print("unfortunately, you are not the winner!")
-}
-
-var classNumber: Int = 10;
-if classNumber >= 5{
-    print("You'r classes start on JAN 10th")
-}else if classNumber < 5{
-    print("classes start on Feb 5")
-}else{
-    print("unknown class number")
-}
