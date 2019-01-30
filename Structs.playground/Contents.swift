452fedb8050cc7e79751f3821c33bbabff815f49
Menhaj <menhaj@Menhajs-MacBook-Pro.local>
2019-01-30 18:18:11 -0500

    commit

--- Structs.playground/Contents.swift
+++ Structs.playground/Contents.swift
@@ -10,7 +10,7 @@ import UIKit
 struct Rectangle {
     var width = 10
     var height = 20
-    
+
     func printDesc(){
         print("The width is \(width) and the height is \(height)")
     }
@@ -23,3 +23,23 @@ var myRectangle = Rectangle()
 myRectangle.printDesc()
 print(myRectangle.doubleWidth())
 
+
+struct Rectangle {
+    var width: Int
+    var height: Int
+    var size: String
+    
+    init(width: Int, height: Int) {
+        self.width = width
+        self.height = height
+        
+        if width + height <= 10 {
+            size = "small"
+        } else {
+            size = "large"
+        }
+    }
+}
+
+let rectangle = Rectangle(width: 400, height: 200)
+print(rectangle.size)
