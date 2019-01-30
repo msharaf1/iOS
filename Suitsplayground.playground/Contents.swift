2037662155b1a0fea139aab3eabef909c59eb4dd
Menhaj <menhaj@Menhajs-MacBook-Pro.local>
2019-01-29 22:16:07 -0500

    commit

--- Suitsplayground.playground/Contents.swift
+++ Suitsplayground.playground/Contents.swift
@@ -1,25 +1,47 @@
-import UIKit
+//import UIKit
+//
+////var str = "Hello, playground"
+//
+//let suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+//let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+//var deckOfCards = [String: [Int]]()
+//// your code here
+//
+//
+//for i in suits{
+//    print(i)
+//    for j in cards {
+//        print(j)
+//    }
+//}
+//
+//for i in 0..<suits.count{
+////    print(suits[i])
+//    deckOfCards[suits[i]] = cards
+//
+//}
+//
+//
+//print(deckOfCards)
 
-//var str = "Hello, playground"
-
-let suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
-let cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-var deckOfCards = [String: [Int]]()
-// your code here
+//https://learnappmaking.com/random-numbers-swift/
 
 
-for i in suits{
-    print(i)
-    for j in cards {
-        print(j)
+func randGen(closeRange: Int) -> Int{
+    var diceRoll = Int.random(in: 1..<closeRange)
+    return diceRoll;
+}
+
+randGen(closeRange: 10)
+
+func tossCoid(tossInt: Int) -> String {
+    if randGen(closeRange: tossInt) % 2 == 0{
+        return "Head"
+    } else {
+        return "Tail"
     }
+
+//    return "";
 }
 
-for i in 0..<suits.count{
-//    print(suits[i])
-    deckOfCards[suits[i]] = cards
-
-}
-
-
-print(deckOfCards)
+print(tossCoid(tossInt: 10))
